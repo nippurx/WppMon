56fe53895c3729f2b0bb5992d8e3130c07d46651
NippurX <nippurx@gmail.com>
2026-02-15 16:01:07 -0300

    Backup 2026-02-15-160107

--- wake_edge_v1.py
+++ wake_edge_v1.py
@@ -7,9 +7,6 @@ pyautogui.FAILSAFE = False
 print("Wake-Edge en ejecución...")
 
 while True:
-    # Click para tomar foco (en la posición actual del mouse)
-    pyautogui.click()
-
     # Movimiento suave para no pegar contra bordes
     pyautogui.moveRel(20, 0, duration=0.2)
     pyautogui.moveRel(-20, 0, duration=0.2)
